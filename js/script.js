0fd4c37062ee8486e3440096305094be290d87be
Michelle Ndiangui <ndianguimichelle@gmail.com>
2023-07-05 01:07:32 +0300

    getting it to work

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@ const board = document.querySelectorAll('input')
 
 board.forEach((move) => {
     move.addEventListener('keydown', () => {
-        if(!(move.value.toUpperCase() === 'X' || move.value.toUpperCase() === 'O')){
+        if (!(move.value.toUpperCase() === 'X' || move.value.toUpperCase() === 'O')) {
             move.value = ""
-        }else{
+        } else {
             let values = []
             let player2
             board.forEach((val) => {
@@ -13,7 +13,14 @@ board.forEach((move) => {
                 }
             })
             player2 = checkUserInput(values[0])
-            getRandomChoice(board, player2)
+            let winner = checkWin(board)
+
+            if (winner) {
+                document.querySelector('#result').innerHTML = declareWinner(winner, player2)
+                board.forEach((key) => key.removeEventListener('keydown', restart))
+            } else {
+                getRandomChoice(board, player2)
+            }
         }
     })
 })
@@ -28,72 +35,94 @@ function checkUserInput(a) {
 function getRandomChoice(arr, choice) {
     let randomIndex = Math.floor(Math.random() * 9)
     console.log(randomIndex);
-    if(!arr[randomIndex].value){
+    if (!arr[randomIndex].value) {
         arr[randomIndex].value = choice
-    }else{
+    } else {
         getRandomChoice(arr, choice)
     }
 }
 
 function checkWin(arr) {
-    let top = arr.slice(2)
-    let middle = arr.slice(3,5)
-    let bottom = arr.slice(-3)
-    
+    let top = []
+    let middle = []
+    let bottom = []
+
+    for (let i = 0; i < arr.length; i++) {
+        if (top.length !== 3) {
+            top.push(arr[i].value)
+        } else if (middle.length !== 3) {
+            middle.push(arr[i].value)
+        } else if (bottom.length !== 3) {
+            bottom.push(arr[i].value)
+        }
+    }
+
     let topWin = checkRow(top)
     let middleWin = checkRow(middle)
     let bottomWin = checkRow(bottom)
 
-    if(!(topWin || middleWin || bottomWin)){
-        if (!(checkColumns(top, middle, bottom))) {
-            if ((checkDiagonal(top, middle, bottom))) {
-                getRandomChoice(arr, choice)
-            }
-        }
-    }else{
-        if(topWin){
-            return topWin
-        }else if(middleWin){
-            return middleWin
-        }else if(bottomWin){
-            return bottomWin
-        }else if (checkColumns(top, middle, bottom)) {
-            return checkColumns(top, middle, bottom)
-        }else if (checkDiagonal(top, middle, bottom)) {
-            return checkDiagonal(top, middle, bottom)
-        }
+    if (topWin) {
+        console.log('Top Bingo');
+        return topWin
+    } else if (middleWin) {
+        console.log('Middle Bingo');
+        return middleWin
+    } else if (bottomWin) {
+        console.log('Bottom Bingo');
+        return bottomWin
+    } else if (checkColumns(top, middle, bottom)) {
+        console.log('Column Bingo');
+        return checkColumns(top, middle, bottom)
+    } else if (checkDiagonal(top, middle, bottom)) {
+        console.log('Diagonal Bingo');
+        return checkDiagonal(top, middle, bottom)
     }
 }
 
 function checkRow(arr) {
-    let value = arr.reduce((acc, c) => {
-       return acc === c ? acc : false
-    })
-    return value
+    if (arr[0]) {
+        let value = arr[0]
+
+        for (let i = 0; i < arr.length; i++) {
+            if (!(arr[i] == value)) {
+                return false
+            }
+        }
+
+        return value
+    }
+
 }
-console.log(checkRow(['A', 'A', 'A'])); 
 
 function checkColumns(a, b, c) {
     for (let i = 0; i < a.length; i++) {
-        if(a[i] === b[i] && a[i] === c[i]){
-            return a[i]
+        if (a[i].value && b[i].value && c[i].value) {
+            if (a[i].value === b[i].value && a[i].value === c[i].value) {
+                return a[i]
+            }
         }
     }
 }
 
-console.log(checkColumns(['A', 'B'], ['A', 'C'], ['A', 'D'])); 
-
 function checkDiagonal(a, b, c) {
-    if((a[0] === b[1] && a[0] === c[2]) || (a[2] === b[1] && a[2] === c[0])){
-        return a[0]
+    if (a[0].value && b[1].value && c[2].value && a[2].value && c[0].value) {
+        if ((a[0].value === b[1].value && a[0].value === c[2].value) || (a[2].value === b[1].value && a[2].value === c[0].value)) {
+            return a[0]
+        }
     }
 }
-console.log(checkDiagonal(['A', 'B', 'D'], ['B', 'A', 'C'], ['A', 'D', 'A'])); 
 
 function declareWinner(winner, player2) {
     if (winner === player2) {
         return 'YOU LOST😓'
-    }else{
+    } else {
         return 'YOU WON!!!!!🎊🎉'
     }
+}
+
+function restart() {
+    board.forEach((box) => {
+        box.value = ""
+        document.querySelector('#result').innerHTML = ""
+    })
 }
