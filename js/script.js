6830ee5976aa84c4db101e78e5b8a724b2b243dd
Michelle Ndiangui <ndianguimichelle@gmail.com>
2023-07-04 23:57:46 +0300

    check for X or O and auto-generate a move

---
+++ js/script.js
@@ -0,0 +1,55 @@
+const board = document.querySelectorAll('input')
+
+board.forEach((move) => {
+    move.addEventListener('keydown', () => {
+        if(!(move.value.toUpperCase() === 'X' || move.value.toUpperCase() === 'O')){
+            move.value = ""
+        }else{
+            let values = []
+            let player2
+            board.forEach((val) => {
+                if (val.value) {
+                    values.push(val.value)
+                }
+            })
+            player2 = checkUserInput(values[0])
+            getRandomChoice(board, player2)
+        }
+    })
+})
+
+function checkUserInput(a) {
+    if (a.toUpperCase() === "X") {
+        return 'O'
+    }
+    return 'X'
+}
+
+function getRandomChoice(arr, choice) {
+    let randomIndex = Math.floor(Math.random() * 9)
+    console.log(randomIndex);
+    if(!arr[randomIndex].value){
+        arr[randomIndex].value = choice
+    }else{
+        getRandomChoice(arr, choice)
+    }
+}
+
+function checkWin(arr) {
+    let top = arr.slice(2)
+    let middle = arr.slice(3,5)
+    let bottom = arr.slice(-3)
+}
+
+function checkRow(arr) {
+    let value = arr.reduce((acc, c) => {
+        if(acc === c ){
+            acc = c
+        }else{
+            return false
+        }
+    })
+    
+    return true
+}
+console.log(checkRow(['A', 'C', 'A'])); 
